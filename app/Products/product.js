e87dc497395ffbd7faf5288d23bec60784246012
Badarinatha <badarinatha@live.com>
2017-07-26 15:13:58 +0100

    Create product.js

---
+++ app/Products/product.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//This code is required if we need to implement Iproduct and have methods defined in interface 
+// calculateDiscount accepts a paramenter percent of type number and returns a value of type number
+//export class Product implements IProduct {
+//constructor(    public productId: number,
+//public productName: string,
+//public productCode: string,
+//public releaseDate: string,
+//public price: number,
+//public description: string,
+//public starRating: number,
+//public imageUrl: string) {}
+//calculateDiscount(percent: number) : number {
+//    return this.price - (this.price * percent / 100 );
+//}
+// } 
+//# sourceMappingURL=product.js.map
